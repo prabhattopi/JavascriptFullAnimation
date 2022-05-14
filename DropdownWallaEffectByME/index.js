ef5721a6117262f418730086968f6c0aebd14cd1
prabhattopi <topi9864@gmail.com>
2022-05-14 23:19:11 +0530

    oijfidsjf

---
+++ DropdownWallaEffectByME/index.js
@@ -0,0 +1,5 @@
+let menuToggle=document.querySelector(".menuToggle")
+let navigation=document.querySelector(".navigation")
+menuToggle.onclick=function(){
+    navigation.classList.toggle("active")
+}
